989d2850f2cf75ea772d2e7d651623da33188036
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-06-24 05:21:54 +0800

    Fourth Commit ============= Added Menu Action Listener in Main Activity and Search View

--- app/src/main/java/com/example/practical4/MainActivity.java
+++ app/src/main/java/com/example/practical4/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.practical4;
 
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.MenuItemCompat.OnActionExpandListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -42,6 +45,97 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this, getString(R.string.settings_clicked), Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, getString(R.string.favorite_clicked), Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_share:
+                Toast.makeText(this, getString(R.string.share_clicked), Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_search:
+                Toast.makeText(this, getString(R.string.search_clicked), Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+//        return super.onCreateOptionsMenu(menu);
+
+        // Get the MenuItem for the action item
+        MenuItem actionMenuSearch = menu.findItem(R.id.action_search);
+
+        // Define the listener
+        actionMenuSearch.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                // Do something when action item collapses
+                Toast.makeText(getBaseContext(), getString(R.string.expand), Toast.LENGTH_LONG).show();
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                // Do something when expanded
+                Toast.makeText(getBaseContext(), getString(R.string.collapse), Toast.LENGTH_LONG).show();
+                return true;  // Return true to expand action view
+            }
+        });
+
+        final SearchView searchView = (SearchView) actionMenuSearch.getActionView();
+
+        searchView.setQueryHint("Hint: try searching = {Hello, World! or Bye, World!}");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                //Do something
+                if (s.equals("Hello, World!")) {
+                    Toast.makeText(getBaseContext(), "Hello, World! 😆😃😃", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.equals("Bye, World!")) {
+                    Toast.makeText(getBaseContext(), "Bye, World! 😆😃😃", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                // do something
+                return false;
+            }
+        });
+
+        return true;
+    }
+
     public void showTimePicker(View view) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
@@ -71,12 +165,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, dateMessage, Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
 
     public void launchAlertDialogActivity(View view) {
