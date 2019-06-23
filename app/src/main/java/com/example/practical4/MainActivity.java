667c32b8c49ec8d977c08b821e32e16c89c73aea
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-06-23 23:10:29 +0800

    Third Commit ============= Fix bugs, toolbar cannot display Added up button toolbar on the AlertDialogActivity

--- app/src/main/java/com/example/practical4/MainActivity.java
+++ app/src/main/java/com/example/practical4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.practical4;
 
+import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 //
 //        ListView listView = (ListView) findViewById(R.id.list_view);
 //        listView.setAdapter(adapter);
+
 
     }
 
@@ -76,23 +78,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                return true;
 
-            case R.id.action_favorite:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
+    public void launchAlertDialogActivity(View view) {
+        Intent intent = new Intent(this, AlertDialogActivity.class);
+        startActivity(intent);
     }
 }
